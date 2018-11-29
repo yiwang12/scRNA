db68010545d26a8cee5e600f2f214b2e13c98e71
Yi Wang <37149810+yiwang12@users.noreply.github.com>
2018-11-28 21:25:47 -0500

    Update functions_mv_plot_v6.R

--- functions_mv_plot_v6.R
+++ functions_mv_plot_v6.R
@@ -1,9 +1,8 @@
-
-
-################################################################################################################################################################################################
+# plot mean-vairance relationship
 source("functions_mv_cal_v3.R")
 library(matrixStats)
 
+# dropout number estimation vs. true dropout number
 plot_compare_drop_num <- function(drop_num_est_in){
   png("compare_drop_num.png")
   plot(drop_num_est_in$drop_num, drop_num_est_in$drop_num_est,main="dropout number of each gene",xlab="true",ylab="estimated")
@@ -11,6 +10,7 @@ plot_compare_drop_num <- function(drop_num_est_in){
   dev.off()
 }
 
+#compare mean-variance relationship before dropout and after dropout
 plot_compare_m_v_beforedrop_est<-function(mean_disp_in,titlename,filename){
   png(paste(filename,"_mean.png",sep="",collapse=""))
   title = paste(titlename," mean comparison",sep="",collapse="")
@@ -24,7 +24,7 @@ plot_compare_m_v_beforedrop_est<-function(mean_disp_in,titlename,filename){
   dev.off()
 }
 
-
+#compare mean-variance relationship before dropout and estimated
 plot_mv<-function(mean_disp_in,titlename,filename){
   png(paste(filename,".png",sep="",collapse=""))
   title = paste(titlename," mean-dispersion",sep="",collapse="")
@@ -34,7 +34,7 @@ plot_mv<-function(mean_disp_in,titlename,filename){
   dev.off()
 }
 
-
+#compare mean-variance relationship lowess curve before dropout and after dropout
 plot_lowess_mv <- function(mean_disp_in,titlename,filename,span){
   png(paste(filename,".png",sep="",collapse=""))
   title = paste(titlename," mean-dispersion",sep="",collapse="")
@@ -44,48 +44,6 @@ plot_lowess_mv <- function(mean_disp_in,titlename,filename,span){
   dev.off()
 }
 
-###### plot all figures
-mv_cal <- function(sim_counts){
-  out=list()
-  drop_num_est = get_est_dropout_num(sim_counts)
-  sim_counts_n_drop = norm_counts_3(sim_counts$drop_counts )
-  sim_counts_n_beforedrop = norm_counts_3(sim_counts$real_counts )
-  mean_disp_knowndrop = mv_knowndrop_norm4(sim_counts,sim_counts_n_beforedrop,sim_counts_n_drop)
-  mean_disp_unknowndrop = mv_unknowndrop_est_3(sim_counts,sim_counts_n_beforedrop,sim_counts_n_drop,drop_num_est$drop_num_est)
-  out$drop_num_est = drop_num_est
-  out$sim_counts_n_drop=sim_counts_n_drop
-  out$sim_counts_n_beforedrop = sim_counts_n_beforedrop
-  out$mean_disp_knowndrop=mean_disp_knowndrop
-  out$mean_disp_unknowndrop=mean_disp_unknowndrop
-  return(out)
-}
-
-
-
-plot_all_figs<-function(sim_counts,mean_disp_knowndrop,mean_disp_unknowndrop,drop_num_est,span_in){
-  plot_mv(mean_disp_in=mean_disp_knowndrop,titlename="(dropout known) ", filename="mv_knowndrop") #known dropout 
-  plot_mv(mean_disp_in=mean_disp_unknowndrop,titlename="(dropout unknown) ",filename= "mv_unknowndrop") #unknown dropout 
-  plot_lowess_mv(mean_disp_in=mean_disp_knowndrop,titlename="(dropout known) ",filename="mv_lowess_knowndrop",span=span_in) #known dropout 
-  plot_lowess_mv(mean_disp_in=mean_disp_unknowndrop,titlename="(dropout unknown) ",filename="mv_lowess_unknowndrop",span=span_in) #unknown dropout 
-  plot_compare_m_v_beforedrop_est(mean_disp_in=mean_disp_knowndrop,titlename="(dropout known) ",filename="compare_knowndrop") #known dropout 
-  plot_compare_m_v_beforedrop_est(mean_disp_in=mean_disp_unknowndrop,titlename="(dropout unknown) ",filename="compare_unknowndrop") #unknown dropout 
-  plot_compare_drop_num(drop_num_est_in=drop_num_est)
-}  
-
-cal_plot<-function(sim_counts,span_in){
-  out=mv_cal(sim_counts)
-  mean_disp_knowndrop=out$mean_disp_knowndrop
-  mean_disp_unknowndrop=out$mean_disp_unknowndrop
-  drop_num_est = out$drop_num_est
-  plot_all_figs(sim_counts,mean_disp_knowndrop,mean_disp_unknowndrop,drop_num_est,span_in)
-}
-
-
-### apply in 10 datasets
-dir.create("/Users/yiwang/Dropbox/yiwang/project_Kasper/3_29/output/Klein")
-setwd("/Users/yiwang/Dropbox/yiwang/project_Kasper/3_29/output/Klein")
-load("/Users/yiwang/Dropbox/yiwang/project_Kasper/2_16/sim_data/Klein_sim_counts4.RData")
-cal_plot(Klein_sim_counts,0.2)
 
 
 
